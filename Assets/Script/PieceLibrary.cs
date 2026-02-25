a0940bfa32e822c87303d9a9b03a242707f76168
N4NH <nanhnotfound@gmail.com>
2026-02-25 13:37:36 +0700

    Update Piece Library va Them asset

--- Assets/Script/PieceLibrary.cs
+++ Assets/Script/PieceLibrary.cs
@@ -23,17 +23,11 @@ public static class PieceLibrary
         // L 4 ô
         new Vector2Int[] { new(0,0), new(0,1), new(0,2), new(1,0) },
 
-        // L 5 ô
-        new Vector2Int[] { new(0,0), new(0,1), new(0,2), new(1,0), new(2,0) },
-
         // 3x3 ô
         new Vector2Int[] { new(0,0), new(0,1), new(0,2), new(1,0), new(1,1), new(1,2), new(2,0), new(2,1), new(2,2) },
 
         // L 4 ô xoay ngang
         new Vector2Int[] { new(0,0), new(0,1), new(1,0), new(2,0) },
-
-        // L 5 ô xoay ngang
-        new Vector2Int[] { new(0,0), new(0,1), new(1,0), new(2,0), new(3,0) },
 
         // L 3 ô đảo
         new Vector2Int[] { new(0,0), new(0,-1), new(-1,0) },
@@ -41,14 +35,8 @@ public static class PieceLibrary
         // L 4 ô đảo
         new Vector2Int[] { new(0,0), new(0,-1), new(0,-2), new(-1,0) },
 
-        // L 5 ô đảo
-        new Vector2Int[] { new(0,0), new(0,-1), new(0,-2), new(-1,0), new(-2,0) },
-
         // L 4 ô xoay ngang đảo
         new Vector2Int[] { new(0,0), new(0,-1), new(-1,0), new(-2,0) },
-
-        // L 5 ô xoay ngang đảo
-        new Vector2Int[] { new(0,0), new(0,-1), new(-1,0), new(-2,0), new(-3,0) },
 
         // L 3 ô xoay đảo
         new Vector2Int[] { new(0,0), new(0,1), new(-1,0) },
@@ -56,17 +44,17 @@ public static class PieceLibrary
         // L 4 ô xoay đảo
         new Vector2Int[] { new(0,0), new(0,-1), new(0,-2), new(1,0) },
 
-        // L 5 ô xoay đảo
-        new Vector2Int[] { new(0,0), new(0,-1), new(0,-2), new(1,0), new(2,0) },
-
         // L 3 ô đảo xoay
         new Vector2Int[] { new(0,0), new(0,-1), new(1,0) },
 
         // L 4 ô đảo xoay
         new Vector2Int[] { new(0,0), new(0,1), new(0,2), new(-1,0) },
 
-        // L 5 ô đảo xoay
-        new Vector2Int[] { new(0,0), new(0,1), new(0,2), new(-1,0), new(-2,0) },
+        //1x1 ô chéo
+        new Vector2Int[] { new(0,0), new(1,1) },
+
+        //1x1 ô chéo đảo
+        new Vector2Int[] { new(0,0), new(1,-1) },
     };
 
     public static Vector2Int[] RandomShape()
