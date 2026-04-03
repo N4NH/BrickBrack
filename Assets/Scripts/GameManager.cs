76cd0459bfa0fb6a787e3332f95fc1132ab768d2
DcsKCS <dcskc2@gmail.com>
2026-04-03 22:10:53 +0700

    Ads

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    [SerializeField] private GoogleAdsMob googleAdsMob;
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (googleAdsMob != null)
+        {
+            googleAdsMob.ReShowBottomBanner();
+        }
+    }
+
+    #region Ads
+    public float GetBottomBannerHeight()
+    {
+        if (googleAdsMob != null)
+        {
+            return googleAdsMob.GetBottomBannerHeight();
+        }
+        return 0;
+    }
+    public void ShowInterstitialAd()
+    {
+        if (googleAdsMob != null)
+        {
+            googleAdsMob.ShowInterstitialAd();
+        }
+    }
+    private void OnApplicationPause(bool paused)
+    {
+        if (!paused && googleAdsMob != null)
+        {
+            googleAdsMob.ShowAppOpenAd();
+        }
+    }
+    #endregion
+
+
+}
